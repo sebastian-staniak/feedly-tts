5822b2ce0ac6e7d90174a4be003dc09d01d735ad
Sebastian Staniak <sstaniak@MacBook-Pro-Sebastian.local>
2016-05-21 22:45:14 +0200

    inital commit

---
+++ symfony/README.md
@@ -0,0 +1,4 @@
+symfony
+=======
+
+A Symfony project created on May 21, 2016, 7:48 pm.
---
+++ symfony/src/FeedlyClient/Application/RestClient.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sstaniak
+ * Date: 21.05.2016
+ * Time: 22:45
+ */
+
+namespace FeedlyClient\Application;
+
+
+class RestClient
+{
+
+}
