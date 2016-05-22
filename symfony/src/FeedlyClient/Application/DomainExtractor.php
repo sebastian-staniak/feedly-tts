dd8fb079dd121903ab96fbd051a803a61e772ec8
sebastian-staniak <sstaniak@MacBook-Pro-Sebastian.local>
2016-05-22 14:29:30 +0200

    no more www.

--- symfony/src/FeedlyClient/Application/DomainExtractor.php
+++ symfony/src/FeedlyClient/Application/DomainExtractor.php
@@ -7,8 +7,8 @@ class DomainExtractor
     public function apply($item)
     {
         $url = $item->origin->htmlUrl;
-        $item->domain = parse_url($url, PHP_URL_HOST);
+        $domain = parse_url($url, PHP_URL_HOST);
 
-        return $item;
+        return str_replace("www.", "", $domain);
     }
 }
